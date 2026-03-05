ae9b208e3dcffb94f2ab313782311eb10d5ce043
qwu5298 <qwu5298@gmail.com>
2026-03-05 15:02:30 +0800

    20260305

--- src/test/java/com/example/WebSocket/WebSocketApplicationTests.java
+++ src/test/java/com/example/WebSocket/WebSocketApplicationTests.java
@@ -1,13 +1,13 @@
 package com.example.WebSocket;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootTest
-class WebSocketApplicationTests {
+@SpringBootApplication
+public class WebSocketApplicationTests {
 
-	@Test
-	void contextLoads() {
+	public static void main(String[] args) {
+		SpringApplication.run(WebSocketApplicationTests.class, args);
 	}
 
 }
